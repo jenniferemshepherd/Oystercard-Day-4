dff64a4c8a34b205bc48b0d82b74c11eaddba93a
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-11 16:50:07 +0000

    inject dependency

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,7 +3,7 @@ require_relative 'station.rb'
 require_relative 'journey.rb'
 
 class Oystercard
-  attr_reader :balance, :entry_station, :exit_station, :journey_history
+  attr_reader :balance, :journey_history
 
   CARD_LIMIT = 90
   MINIMUM_FARE = 1
@@ -12,6 +12,7 @@ class Oystercard
     @balance = 0
     @journey_history = []
     @journey = journey_class
+    @current_journey
   end
 
   # def active?
@@ -25,23 +26,19 @@ class Oystercard
 
   def touch_in(station)
     raise "you dont have enough money" if @balance < MINIMUM_FARE
-    @journey = Journey.new
-    raise "you have already touched in" if @journey.active?
-    @journey.set_entry(station)
-    @entry_station = station
+    @current_journey = @journey.new
+    @current_journey.set_entry(station)
   end
 
   def touch_out(station)
-    @journey = Journey.new if @journey == nil
-    # @journey.set_entry(nil) if @journey.entry != nil
-    raise "you are not touched in" unless @journey.active?
+    @current_journey = @journey.new  if @current_journey == nil
+    @current_journey.set_exit(station)
     deduct(MINIMUM_FARE)
-    @exit_station = station
     add_journey_to_history
   end
 
   def add_journey_to_history
-    @journey_history << @journey.combine
+    @journey_history << @current_journey.combine
   end
 
   private
